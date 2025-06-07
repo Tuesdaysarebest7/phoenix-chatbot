db0537450e9fd499984bd9850e23cdb472090e21
Tuesdaysarebest7 <108067222+Tuesdaysarebest7@users.noreply.github.com>
2025-06-07 12:44:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def home():
     return "Phoenix is online 🔥", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=10000)
